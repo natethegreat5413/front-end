2f40a2b4cfaa90308971adc7e5acf899f7a0b980
Nathan Cowley <nathancowley12@gmail.com>
2020-05-24 14:59:16 -0600

    ive got the skeleton for both forms, working on validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+
 const Login = (props) => {
   const [account, setaccount] = useState({
     username: "",
@@ -25,10 +27,35 @@ const Login = (props) => {
         props.history.push("/");
       });
   };
+  
   return (
     <div>
       <form className='form' onSubmit={Submitform}>
-hi there
+        <h3>Welcome back!</h3>
+        <h4>Log into your account</h4>
+        <label htmlFor="username">
+          Username
+          <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          />
+        </label><br/>
+
+        <label htmlFor="password">
+          Password
+          <input
+          type="text"
+          name="password"
+          placeholder="Password"
+          />
+        </label>
+
+        <button>Next</button>
+
+        <h4>Not registered yet?  Register Now!</h4>
+
+        <Link to="/Register">Register</Link>
       </form>
     </div>
   );
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,69 @@
 import React, { useState } from "react";
 import axios from "axios";
+import * as yup from "yup"
+import { Link } from "react-router-dom"
+import Login from "./Login"
+
+const formSchema = yup.object().shape({
+  username: yup.string().required("Must put Username"),
+  email: yup.string().required("Must have valid Email"),
+  password: yup.string().required("Must have Password")
+})
+
 const Register = (props) => {
- 
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+});
+
+  const [errors, setErrors] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const inputChange = e => {
+    e.persist();
+  }
+
   return (
     <div>
-      <form className='form' >
-      Register
+      <form className='form'>
+        <h3>Let's get started!</h3>
+        <h4>Create your account</h4>
+        <label htmlFor="username">
+          Username
+          <input
+            type="text"
+            name="username"
+            placeholder='Username'
+          />
+        </label><br/>
+
+        <label htmlFor="email">
+          Email
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+          />
+        </label><br/>
+
+        <label htmlFor="password">
+          Password
+          <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          />
+        </label><br/>
+
+        <button>Next</button><br/>
+
+        <h4>Already have an Account? Login Here!</h4>
+
+        <Link to="/">Login</Link>
       </form>
     </div>
   );
